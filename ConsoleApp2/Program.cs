2abc28f6337d86c250f68556bffced852f8254b1
Benjamin <benjamin.nomine@outlook.fr>
2020-09-28 08:29:51 -0400

    Nettoyage final

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,15 +1,4 @@
-﻿using Clinique.Domain.Models;
-using Clinique.Domain.Enums;
-using System;
-using Microsoft.EntityFrameworkCore.Storage;
-using Clinique.EntityFramework.Services;
-using Clinique.Domain.Services;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
-using Clinique.AspNetCore.Models;
-using Clinique.Domain.Services.API;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Threading.Tasks;
 using Clinique.EntityFramework.Seed;
 using Clinique.EntityFramework;
 using System.Threading;
